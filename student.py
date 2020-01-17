1deb34e39be2033697d9f0b0eed04232bf1288bf
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-17 14:05:20 -0600

    set up remaining files for class types

---
+++ student.py
@@ -0,0 +1,19 @@
+# You must define a custom type for representing a student in code. A student can only be in one cohort at a time. A student can be working on many exercises at a time.
+
+# Properties
+# First name
+# Last name
+# Slack handle
+# The student's cohort
+# The collection of exercises that the student is currently working on
+
+class Student:
+
+    def __init__(self, first_name, last_name, slack_handle, student_cohort, student_exercises):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack_handle = slack_handle
+        self.student_cohort = student_cohort
+        self.student_exercises = list()
+
+    
